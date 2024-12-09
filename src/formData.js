bfc7e04353f2cc73139fc263a22395064f815061
Florian <floriangeorges1992@gmail.com>
2024-12-09 14:13:35 +0100

    add formdata

--- src/formData.js
+++ src/formData.js
@@ -1,1 +1,19 @@
-console.log("formData");
+//formadata avec index.html
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  console.log(formData);
+  formData.append("name", "123");
+  formData.set("email", "tintin@gmail.com");
+  console.log(formData.get("email"));
+
+  for (let pair of formData) {
+    console.log(pair);
+  }
+  fetch("/test", {
+    method: "POST",
+    body: formData,
+  });
+});
